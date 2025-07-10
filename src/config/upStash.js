3a238364afaeccc8c68237a07f96266c2c0a4697
Karan Narode <63549941+ikarannarode@users.noreply.github.com>
2025-07-10 17:11:20 +0530

    Update upStash.js

--- src/config/upStash.js
+++ src/config/upStash.js
@@ -1,13 +1,11 @@
 import { Redis } from "@upstash/redis";
 import { Ratelimit } from "@upstash/ratelimit";
-import "dotenv/config";
 
-// Ensure environment variables are available
 const redis = Redis.fromEnv();
 
 const ratelimit = new Ratelimit({
   redis,
-  limiter: Ratelimit.slidingWindow(200, "60 s"), // 200 requests per 60 seconds
+  limiter: Ratelimit.fixedWindow(100, "1 m"), //no scripting used here
 });
 
 export default ratelimit;
